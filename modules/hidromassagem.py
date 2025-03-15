bbd5bc0645314b08c8bef396b722930bdd5f3683
EngineerBR90 <reinaldo.vfarias@gmail.com>
2025-03-15 13:20:36 -0300

    Update hidromassagem.py

--- modules/hidromassagem.py
+++ modules/hidromassagem.py
@@ -281,7 +281,8 @@ def run():
                     **Sugestões:**
                     - Verifique se a pressão selecionada está correta
                     - Considere dividir em dois ou mais sistemas com acionamentos independentes
-                    - Considere utilizar múltiplas MBs em paralelo. Para tal é imprescindível dimensionar linha de sucção e verificar velocidade de fluxo ≤1,80 m/s)
+                    - Considere utilizar múltiplas MBs em paralelo. Para tal é imprescindível dimensionar 
+                    linha de sucção e verificar velocidade de fluxo ≤1,80 m/s.
                     - Verifique modelos com maior capacidade
                     """)
 
